d91d9b7a7d4ef04a52a187a3cc34bfb7e2a9b3ac
deepu <deepumahapatro2006@gmail.com>
2026-05-18 17:03:43 +0530

    FIND MISSING NUMBER

---
+++ ARRAYS_EASY/PROBLEM6.py
@@ -0,0 +1,52 @@
+nums=[1,2,3,5]
+n=5
+if n<=0:
+    print("invalid number")
+elif len(nums)!=n-1:
+    print("invalid array")
+else:
+    result=n*(n+1)//2
+    sum=sum(nums)
+    missing=result-sum
+print("missing number:",missing)  
+
+#USING LOOP METHOD
+nums=[1,2,3,5]
+n=5
+if n<=0:
+    print("invalid array")
+else:
+    for i in range(1,n+1):
+        if i not in nums:
+            print("missing number:",i)
+            
+#USING HASH SET METHOD
+nums=[1,2,3,5]
+n=5
+if n<=0:
+    print("invalid array")
+else:
+    hash_set=set(nums)
+    for i in range(1,n+1):
+        if i not in hash_set:
+            print("missing number:",i)
+            
+#USING XOR METHOD
+arr = [1,2,4,5]
+n = 5
+# Edge Case
+if n <= 0:
+    print("Invalid n")
+elif len(arr) != n - 1:
+    print("Invalid array size")
+else:
+    xor1 = 0
+    xor2 = 0
+    # XOR from 1 to n
+    for i in range(1, n + 1):
+        xor1 ^= i
+    # XOR array elements
+    for num in arr:
+        xor2 ^= num
+    missing = xor1 ^ xor2
+    print("Missing Number:", missing)

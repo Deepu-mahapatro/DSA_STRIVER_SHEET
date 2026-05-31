640f6f15cbdf5b12141fdc5c752a29276ae854ea
deepu <deepumahapatro2006@gmail.com>
2026-05-31 12:20:05 +0530

    SET MATRIX ZEROS

--- ARRAYS_MEDIUM/PROBLEM10.py
+++ ARRAYS_MEDIUM/PROBLEM10.py
@@ -62,3 +62,38 @@ matrix=[
     [1,1,1]
 ]
 print(set_zeros(matrix))
+
+#OPTIMAL METHOD
+def set_zeroes(matrix):
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    row_zero = [False] * rows
+    col_zero = [False] * cols
+
+    # FIND ALL ORIGINAL ZEROS
+    for i in range(rows):
+        for j in range(cols):
+
+            if matrix[i][j] == 0:
+                row_zero[i] = True
+                col_zero[j] = True
+
+    # UPDATE MATRIX
+    for i in range(rows):
+        for j in range(cols):
+
+            if row_zero[i] or col_zero[j]:
+                matrix[i][j] = 0
+
+    return matrix
+
+
+matrix = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+]
+
+print(set_zeroes(matrix))
